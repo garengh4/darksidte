9b36f4d4c3a8cf334f597cf83414ea1b972419dd
Garen <garengh4@gmail.com>
2022-11-06 17:12:12 -0500

    start fixing layout for all pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -22,32 +22,35 @@
 <section>
   <div class="container">
     <div id="leftcolumn">
-      <div id="portrait">
-        <img src="../images/portrait.jpg" alt="portrait" />
-      </div>
       <div class="info">
-        <h1>Hello</h1>
+        <h1>Hello!</h1>
+        <p>My name is Garen and I am a software developer.</p>
+        <br />
         <p>
-          I am a software developer in the Java-Springboot-Angular framework.
+          In my work I am usually assigned tasks like fixing this
+          <button>button</button> or initializing api-documentation for the entire
+          Java service.
         </p>
+        <br />
         <p>
-          I look at why this clicky <button>button</button> is not downloading.
+          Most times I get to go into the Java backend to see whats up with the
+          database. I think its pretty cool!
         </p>
+
+        <br />
         <p>
-          If it doesn't make any changes to your information, I can fix it in a
-          day probably.
+          In my free time I learn other frameworks that accomplish the same
+          tasks but in a totally unique way.
         </p>
+        <br />
         <p>
-          Otherwise I am going to have to go into the smudgy ball we call
-          springboot and trace the APIs.
+          Sometimes humans truly amaze me, and I can forget we are horrible
+          creatures destroying the planet lmao.
         </p>
       </div>
-
-      <!-- <p>
-        One time I had to initialize api documentation on the project by the end
-        of the sprint and
-      </p> -->
-      <br />
+    </div>
+    <div id="midcolumn">
+      <img src="../images/portrait.jpg" alt="portrait" />
     </div>
     <div id="rightcolumn">Table of contents, socials</div>
   </div>
@@ -59,33 +62,34 @@
     position: relative;
     display: flex;
     align-items: left;
-    gap: 10rem;
+    gap: 5rem;
 
     display: grid;
     height: 100vh;
-    grid-template-columns: 2.5fr 1.25fr;
+    grid-template-columns: 2.5fr 2fr 1.25fr;
     grid-template-rows: 2.75fr 1.25fr;
-    grid-template-areas: "leftcolumn rightcolumn";
-    font-weight: 800;
-    font-size: 20px;
+    grid-template-areas: "leftcolumn midcolumn rightcolumn";
+
     border-radius: 5rem;
-    padding: 5rem 10rem 5rem 5rem;
+    padding: 1rem;
     background-color: palegoldenrod;
   }
 
   #leftcolumn {
+    font-weight: 800;
+    font-size: 20px;
     background: lightblue;
     grid-area: leftcolumn;
     border-radius: 20px;
-    padding-top: 10px;
+    line-height: 2.5rem;
+    padding: 2rem;
   }
-  #portrait {
-    background: lightslategray;
+
+  #midcolumn {
+    background: lightpink;
+    grid-area: midcolumn;
     border-radius: 20px;
-    padding: 10px 10px 10px 10px;
-    float: left;
-    /* width:1px;
-    height: auto; */
+    padding: 10px;
   }
 
   #rightcolumn {
@@ -96,6 +100,6 @@
   }
   .info {
     font-size: x-large;
-    padding: 100px 100px 600px;
+    /* padding: 100px 100px 600px; */
   }
 </style>
