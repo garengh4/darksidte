b529db3867e3420356d391538f10e621d632ab7c
Garen <garengh4@gmail.com>
2022-11-24 16:20:03 -0500

    fixed adapter-2

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,10 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
-	kit: {
-		adapter: adapter()
-	}
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter({ out: 'my-output-directory' })
+  }
 };
 
 export default config;
