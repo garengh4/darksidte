c0eb3c51ee71ed2f9e7b29d44ceff5526b4dbc9f
Garen <garengh4@gmail.com>
2022-11-25 13:06:08 -0500

    changed to adapter from swa to node

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
-import azure from 'svelte-adapter-azure-swa';
+// import azure from 'svelte-adapter-azure-swa';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    adapter: azure()
+    adapter: adapter()
   }
 };
 
