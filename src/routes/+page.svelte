a7bb0901b493c2c2e6d549a8d2c5273a133e5f81
Garen <garengh4@gmail.com>
2022-12-02 15:47:26 -0500

    clean

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,75 +52,17 @@
     <h2>My current projects!</h2>
   </div>
 
-  <div class="test">
     <div class="project-component">
       {#each config.projects as project}
         <ProjectComponent {project} />
       {/each}
     </div>
-  </div>
 
 </section>
 
 <!-- #################################################################################################### -->
 <style>
-  /* ============================================================= */
-  .test {
-    align-items: center;
-    justify-content: center;
-    display: flex;
-    flex-direction: column;
-  }
-  .project-component {
-    display: flex;
-    gap: 3em;
-    flex-direction: column;
-    align-items: center;
-    /* margin-top: 3em; */
-    margin-bottom: 5em;
-  }
-  /* ====================================================== */
-  #heading {
-    grid-area: d;
-    margin-bottom: 5em;
-    font-size: small;
-    text-align: center;
-
-  }
-
-  /* =====================//TODO: small @media bug needs fixing======================== */
-  .framework-container {
-    display: flex;
-    flex-wrap: nowrap;
-    overflow-x: auto;
-    gap:1em;
-    margin-top: -3em;
-    justify-content: center;
-    margin-bottom: 8em;
-
-  }
-  /* ====================================================== */
-
-  #box-hi{
-    grid-area: a;
-    font-size: 20.5pt;
-    margin-top: -3em;
-  }
-
-  #description {
-    grid-area: c;
-    font-size: small;
-    line-height: 1.5em;
-  }
-  #fullbody > img {
-    width: 150px;
-    margin-top: -8em;
-  }
-  #fullbody {
-    grid-area: b;
-  }
-
-  .grid-template-columns {
+    .grid-template-columns {
     display: grid;
     grid-template-columns: 10em auto;     /* @media */
     grid-template-rows: auto auto;
@@ -134,37 +76,51 @@
     margin-bottom: 5em;
     gap: 1em;
   }
+  #box-hi{
+    grid-area: a;
+    font-size: 20.5pt;
+    margin-top: -3em;
+  }
+  #description {
+    grid-area: c;
+    font-size: small;
+    line-height: 1.5em;
+  }
+  #fullbody {
+    grid-area: b;
+  }
+  #fullbody > img {
+    width: 150px;
+    margin-top: -8em;
+  }
+  #heading {
+    grid-area: d;
+    margin-bottom: 5em;
+    font-size: small;
+    text-align: center;
 
-  /* //TODO: SOMETHING BROKE HERE BETWEEN LAPTOP AND TABLET */
-  /* @media screen and (min-width: 760px) {
-    .grid-template-columns {
-      grid-template-columns: 100em auto;
-    }
-    #title {
-      font-size: xx-large;
-      margin-top: -1em;
-    }
-    #description {
-      font-size: medium;
-      margin-top: -2em;
-    }
-    #fullbody > img {
-      width: 200px;
-      margin-top: -5em;
-    }
-    #heading {
-      font-size: large;
-      margin-bottom: 3em;
-    }
+  }
 
-    .project-component {
-      align-items: center;
-      justify-content: center;
-      width: 20em;
-    }
     .framework-container {
-      gap: 2em;
-    } 
-  }*/
-  /* ===================================================================== */
+    display: flex;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+    gap:1em;
+    margin-top: -3em;
+    justify-content: center;
+    margin-bottom: 8em;
+
+  }
+  .project-component {
+    display: flex;
+    gap: 3em;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 5em;
+  }
+
+  @media screen and (min-width: 768px) {
+
+  }
+ 
 </style>
