c1d426088074668630fa10bfae735f68af4971cb
Garen <garengh4@gmail.com>
2022-12-02 13:46:27 -0500

    iphone12 complete

--- src/lib/project-component.svelte
+++ src/lib/project-component.svelte
@@ -7,43 +7,65 @@
   <div class="container">
     <div class="display">
       {#if project.background}
-      <img src="{project.background}" alt="{project.project}">
+        <img src={project.background} alt={project.project} />
       {:else}
-      <img src="/images/default-background-1.jpg" alt="DEFAULT BACKGROUND 1">
+        <img
+          src="/images/default-background-1.jpg"
+          alt="DEFAULT BACKGROUND 1"
+        />
       {/if}
     </div>
     <div class="console">
       <h3>{project.project}</h3>
       <span>{project.description}</span>
-      <div class="links">
-        <a class="btn" href="{project.repo}">Source code</a>
-        {#if project.link}
-          <a class="btn" href="{project.link}">Live Project</a>
-        {/if}
-
-      </div>
-
+      
     </div>
-  </div>
+    <div class="links">
+      <a href={project.repo}>Source code</a>
+      {#if project.link}
+        <a href={project.link}>Live Project</a>
+      {/if}
+    </div>
 
-  
+  </div>
 </section>
 
 <style>
+  .links {
+    text-align: center;
+  }
+  a {
+    /* background-color: #4caf50; /* Green */
+    color: white;
+    padding: 10px 30px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    border-radius: 4px;
+    transition-duration: 0.4s;
+    opacity: 0.6;
+    background-color: #555;
+  }
+  a:hover {
+    background-color: darkred;
+    color: bisque;
+    opacity: 1
+  }
   .container {
     width: 20em;
   }
-  
+
   .console {
     background: rgba(53, 53, 53, 0.47);
     backdrop-filter: blur(62px);
     color: white;
     padding: 1.5em;
   }
-  img{
+  img {
     width: 20em;
   }
-  
+
   h3 {
     font-size: 18pt;
     margin-bottom: 1em;
@@ -52,8 +74,5 @@
   span {
     line-height: 1.5em;
     font-size: small;
-    /* font-family: Poppins,Helvetica,system-ui; */
   }
-
-  
 </style>
