91e49090d2d165f257652ac60a9ddac3d0efd176
Garen <garengh4@gmail.com>
2022-12-02 12:01:10 -0500

    pre style button

--- src/lib/project-component.svelte
+++ src/lib/project-component.svelte
@@ -15,59 +15,25 @@
     <div class="console">
       <h3>{project.project}</h3>
       <span>{project.description}</span>
+      <div class="links">
+        <a class="btn" href="{project.repo}">Source code</a>
+        {#if project.link}
+          <a class="btn" href="{project.link}">Live Project</a>
+        {/if}
+
+      </div>
+
     </div>
   </div>
 
-  <!-- <div class="container">
-    {#if project.background}
-      <div
-        class="display"
-        style="background-image: url('{project.background}');"
-      >
-        {#if project.logo}
-          <img class="logo" src={project.logo} alt={project.project} />
-        {:else}
-          <img
-            class="nologo"
-            src="/images/default-logo-1.svg"
-            alt="HELLO WORLD"
-          />
-        {/if}
-      </div>
-    {:else}
-      <div
-        class="display"
-        style="background-image: url('/images/default-background-2.jpg');"
-      />
-    {/if}
-
-    <div class="info">
-      <h3>{project.project}</h3>
-      <span>{project.description}</span>
-    </div>
-  </div> -->
-
-  <!-- Project Links -->
-  <!-- {#if project.link || project.repo}
-    <div class="links">
-      {#if project.link}
-        <a href={project.link} target="__blank">Live Project</a>
-      {/if}
-      {#if project.repo}
-        <a href={project.repo} target="__blank">Source Code</a>
-      {/if}
-    </div>
-  {/if} -->
+  
 </section>
 
 <style>
   .container {
-    /* background-color: blue; */
     width: 20em;
   }
-  .display {
-    /* background-position:50% 50%; */
-  }
+  
   .console {
     background: rgba(53, 53, 53, 0.47);
     backdrop-filter: blur(62px);
@@ -76,36 +42,8 @@
   }
   img{
     width: 20em;
-
   }
-  .nologo {
-    width: 10em; /* @media */
-    filter: blur(5px);
-  }
-
-  .logo {
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 20em; /* @media */
-    padding: 2em;
-    /* background-color: red; */
-  }
-
-  /* .container > .project {
-    border-top-left-radius: 1em;
-    border-top-right-radius: 1em;
-    background-color: blue;
-  } */
-  .info {
-    background: rgba(53, 53, 53, 0.47);
-    backdrop-filter: blur(62px);
-    color: white;
-    padding: 1.5em;
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
-    width: 20em; /* @media */
-  }
+  
   h3 {
     font-size: 18pt;
     margin-bottom: 1em;
@@ -117,12 +55,5 @@
     /* font-family: Poppins,Helvetica,system-ui; */
   }
 
-  /* @media screen and (min-width: 750px) {
-    .container > .project > img {
-      width: 20em;
-    }
-    .container > .project {
-      width: 40em;
-    }
-  } */
+  
 </style>
